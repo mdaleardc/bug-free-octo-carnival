003c632d1de4b9f92099f6c2ca2c62c4970c8d1b
mdaleardc <ismailaliamot@gmail.com>
2025-03-29 05:36:51 +0600

    img otimizatio enabled

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   devIndicators: false,
-  images: {
-    unoptimized: true, // Disable image optimization to avoid sharp issues
-  },
 };
 
 export default nextConfig;
