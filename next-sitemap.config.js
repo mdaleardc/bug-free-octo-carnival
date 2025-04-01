32af0e641e1dbce4899bddbf38c2706d30e88cf4
mdaleardc <ismailaliamot@gmail.com>
2025-04-02 04:38:04 +0600

    sitemap imporvement

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-// next-sitemap.js
-module.exports = {
-  siteUrl: 'https://myanmarbookshub.netlify.app',
-  generateRobotsTxt: true, // Optional: to generate robots.txt
-  sitemapSize: 7000,
-  changefreq: 'daily', // You can set different frequency for different pages.
-  priority: 1.0, // Adjust according to importance.
-};
